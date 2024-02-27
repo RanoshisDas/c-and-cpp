9b12010f666284e6e4d8145e2a2c6477da7cce88
Ranoshis Das <127428253+RanoshisDas@users.noreply.github.com>
2024-02-27 23:40:58 +0530

    updated files

--- 21stickgame.cpp
+++ 21stickgame.cpp
@@ -1,28 +1,31 @@
 #include<stdio.h>
-int player1(int stick,int n){
-	printf("\nChoice in between 1 to 4\n-> ");
+int player1(int stick){
+    int n;
+	printf("\nChoice in between 1 to 4 -> ");
 	scanf("%d",&n);
+
 	if(n>4||n<1){
 		printf("\nWrong input...!!!");
-		player1(stick,n);
+		n=player1(stick);
 	}
 	else{
 		stick=stick-n;
 	}
 	if(stick==0){
 		printf("\nWrong input...!!!");
-		player1(stick+n,n);
+		n=player1(stick+n);
 	}
 	return n;
 }
 int main(){
-	int stick=21,c,n,choice;
-    printf("1.You play first\n2.Computer play first\nEnter choice:");
+	static int stick=21;
+    int c,n,choice;
+    printf("we have 21 stick\n1.You play first\n2.Computer play first\nEnter choice:");
     scanf("%d",&choice);
     while(1){
         switch (choice) {
             case 1:{
-                n=player1(stick,n);
+                n=player1(stick);
                 stick=stick-n;
                 printf("\nYou pick:%d\nstick left: %d",n,stick);
                 c=5-n;
@@ -30,7 +33,8 @@ int main(){
                 stick=stick-c;
                 printf("\nNumber of stick left: %d",stick);
                 if(stick==1){
-                    printf("!!!...Last stick remain...You Lost...!!!");
+                    printf("\n\t!!!...Last stick remain...You Lost...!!!\n");
+                    stick=0;
                     break;
                 }
                 break;}
@@ -39,11 +43,12 @@ int main(){
                 printf("\nNumber of stick computer pick: %d",c);
                 stick=stick-c;
                  printf("\nNumber of stick left: %d",stick);
-                n=player1(stick,n);
+                n=player1(stick);
                 stick=stick-n;
                 printf("\nYou pick:%d\nstick left: %d",n,stick);
                 if(stick==1){
-                    printf("!!!...Last stick remain...You Win...!!!");
+                    printf("\n\t!!!...Last stick remain...You Win...!!!\n");
+                    stick=0;
                     break;
                 }
                 if ((c+n)%5!=0) {
@@ -53,7 +58,11 @@ int main(){
                  printf("\nNumber of stick left: %d",stick);
                     choice=1;
                 }
-                break;}
+                break;
+                }
+        }
+        if(stick==0){
+            break;
         }
     }
 	return 0;
