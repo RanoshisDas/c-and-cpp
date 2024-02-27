32c5ca7d4103c69ea4a34585666c1eef4c5cb709
Ranoshis Das <127428253+RanoshisDas@users.noreply.github.com>
2024-02-27 21:18:34 +0530

    Update 21stickgame.cpp

--- 21stickgame.cpp
+++ 21stickgame.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
 int player1(int stick,int n){
-	//int n;
 	printf("\nChoice in between 1 to 4\n-> ");
 	scanf("%d",&n);
 	if(n>4||n<1){
@@ -17,19 +16,45 @@ int player1(int stick,int n){
 	return n;
 }
 int main(){
-	int stick=21,c,n;
-	while(1){
-		n=player1(stick,n);
-		stick=stick-n;
-		printf("\nYou pick:%d\nstick left: %d\n",n,stick);
-		c=5-n;
-		printf("\nNumber of stick computer pick: %d\n",c);
-		stick=stick-c;
-		printf("\nNumber of stick left: %d\n",stick);
-		if(stick==1){
-			printf("!!!...You Lost...!!!");
-			break;
-		}
-	}
+	int stick=21,c,n,choice;
+    printf("1.You play first\n2.Computer play first\nEnter choice:");
+    scanf("%d",&choice);
+    while(1){
+        switch (choice) {
+            case 1:{
+                n=player1(stick,n);
+                stick=stick-n;
+                printf("\nYou pick:%d\nstick left: %d",n,stick);
+                c=5-n;
+                printf("\nNumber of stick computer pick: %d",c);
+                stick=stick-c;
+                printf("\nNumber of stick left: %d",stick);
+                if(stick==1){
+                    printf("!!!...Last stick remain...You Lost...!!!");
+                    break;
+                }
+                break;}
+            default:{
+                c=1;
+                printf("\nNumber of stick computer pick: %d",c);
+                stick=stick-c;
+                 printf("\nNumber of stick left: %d",stick);
+                n=player1(stick,n);
+                stick=stick-n;
+                printf("\nYou pick:%d\nstick left: %d",n,stick);
+                if(stick==1){
+                    printf("!!!...Last stick remain...You Win...!!!");
+                    break;
+                }
+                if ((c+n)%5!=0) {
+                    c=5-(c+n)%5;
+                    printf("\nNumber of stick computer pick: %d",c);
+                stick=stick-c;
+                 printf("\nNumber of stick left: %d",stick);
+                    choice=1;
+                }
+                break;}
+        }
+    }
 	return 0;
 }
